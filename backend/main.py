0ac14f47119b22c4860b0f988ac79e22a2e7e3f7
Aliya Rasheed <152678090+aliyaarasheed@users.noreply.github.com>
2026-06-18 12:25:12 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.database import Base, engine
 
 from app.routes.products import router as product_router
+from app.routes.orders import router as order_router
 from app.routes.dashboard import router as dashboard_router
 
 Base.metadata.create_all(bind=engine)
@@ -11,10 +13,17 @@ app = FastAPI(
     title="Product Management Dashboard API"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(product_router)
 app.include_router(order_router)
 app.include_router(dashboard_router)
-
 
 @app.get("/")
 def root():
